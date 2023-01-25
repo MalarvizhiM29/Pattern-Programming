b35c6fc100d351eee6d5c1c1f8fd39524d5ef6b2
MALARVIZHI M <Malarm2004@gmail.com>
2023-01-26 00:10:35 +0530

    7th program

---
+++ program7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    int k = 1;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", k);
+            k++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
