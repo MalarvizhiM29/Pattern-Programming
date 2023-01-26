8e9f872e4c5acd83b42d1be460fa5d1ee3144f6f
MALARVIZHI M <Malarm2004@gmail.com>
2023-01-26 23:49:13 +0530

    9th program

--- program9.c
+++ program9.c
@@ -4,16 +4,16 @@ void main()
 {
     for (int i = 1; i <= 5; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = i; j >= 1; j--)
         {
-            printf("1 ");
+            printf("%d ", j);
         }
         printf("\n");
     }
 }
 
 // 1
-// 1 1
-// 1 1 1
-// 1 1 1 1
-// 1 1 1 1 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// 5 4 3 2 1
